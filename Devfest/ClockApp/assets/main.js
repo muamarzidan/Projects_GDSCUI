efd0e3351aa3be3e28bafc16c44f90c52a63a1ea
muamarzidan <codeofomiru11@gmail.com>
2023-12-04 16:51:04 +0700

    feat: add ClockApp challenge

--- Devfest/ClockApp/assets/main.js
+++ Devfest/ClockApp/assets/main.js
@@ -1,24 +1,20 @@
-function updateClock() {
+function clockTime() {
     var date = new Date();
-    var hours = date.getHours();
-    var minutes = date.getMinutes();
-    var seconds = date.getSeconds();
     
-    var time = `${hours < 10 ? "0" : ""}${hours}:${minutes < 10 ? "0" : ""}${minutes}:${seconds < 10 ? "0" : ""}${seconds}`;
+    var hours = String(date.getHours()).padStart(2, "0");
+    var minutes = String(date.getMinutes()).padStart(2, "0");
+    var seconds = String(date.getSeconds()).padStart(2, "0");
+    var time = `${hours}:${minutes}:${seconds}`;
     var clock = document.getElementById("clock");
     clock.innerHTML = time;
-}
-function updateDate()
-{
-    var date = new Date();
-    var dayOfWeek = date.toLocaleString('en-US', { weekday: 'long' });
+    
+    var dayPerWeek = date.toLocaleString('en-US', { weekday: 'long' });
     var month = date.toLocaleString('en-US', { month: 'long' });
     var day = date.getDate();
     var year = date.getFullYear();
-    var dateInfo = `${dayOfWeek}, ${month} ${day}, ${year}`;
-    var days = document.getElementById("date");
-    days.innerHTML = dateInfo;
+    var dateInfo = `${dayPerWeek} | ${month} | ${day} | ${year}`;
+    var dateElement = document.getElementById("date");
+    dateElement.innerHTML = dateInfo;
 }
 
-setInterval(updateClock, 1000);
-setInterval(updateDate, 1000);
+setInterval(clockTime, 1000);
